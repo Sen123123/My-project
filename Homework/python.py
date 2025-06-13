b36dfc9e37db7c5952ec9bbf892cbdce13308b47
Sen123123 <usen.summerfield@gmail.com>
2025-06-13 14:40:16 +0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Homework/python.py

--- Homework/python.py
+++ Homework/python.py
@@ -1,8 +1,10 @@
 Alp = input("Write a sentence:")
 count = 0
 
+vowels = "aeiouAEIOU"
+
 for char in Alp:
-    if char.isalpha():
+    if char in vowels:
         count += 1
 
 print("Number of characters/ alphabets you have typed:",count)
